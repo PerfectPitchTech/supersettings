b02cb6a3e27c3e47de670c3f9baa595f38981fc9
gdoermann <gdoermann@perfectpitchtech.com>
2016-06-15 22:05:27 -0600

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'gdoermann'
 from distutils.core import setup
 
 setup(name='supersettings',
-      version='0.2.0',
+      version='0.3.0',
       description='Super settings config.  Merge many settings config files into one configuration!',
       author='Greg Doermann',
       author_email='gdoermann@gmail.com',
